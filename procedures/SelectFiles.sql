902a8d9777c53902ff81a4489f052c10e3f58462
Kieran Finn <kieran.finn@hotmail.com>
2022-07-24 11:57:49 +0100

    first commit

---
+++ procedures/SelectFiles.sql
@@ -0,0 +1,18 @@
+
+DECLARE @ProcedureName AS VARCHAR(100)='SelectFiles'
+
+
+DROP PROCEDURE IF EXISTS @ProcedureName;
+
+DELIMITER //
+
+CREATE PROCEDURE @ProcedureName()
+BEGIN
+    select mf.Fileid, mf.URL, mf.UploadDate, u.IPAddress
+    From MediaFiles mf
+    left join
+    Users u
+on mf.OwnerID=u.UserID;
+END
+
+DELIMITER ;
